cf41af1d9f50b7c64d8128670b0bfcb06bc2085c
ozayn <a0wave0a@gmail.com>
2015-04-24 18:09:51 -0700

    Pins Users and Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,17 @@
+# a pin belongs to a user
 class Pin < ActiveRecord::Base
+	belongs_to :user
+end
+
+# a user has many pins
+
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :pins
 end
