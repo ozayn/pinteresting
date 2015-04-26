ea66bd8a548b8c419d2c620f2f48bafd05988cb7
ozayn <a0wave0a@gmail.com>
2015-04-26 05:39:46 -0700

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   # get 'pages/home'
-  root "pages#home"  # creates root_path
+  #root "pages#home"  # creates root_path
+  root "pins#index"
   get "about" => "pages#about" #about_path is created
   
   # The priority is based upon order of creation: first created -> highest priority.
